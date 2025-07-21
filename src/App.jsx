1d9fd48044b13f92fdfaa454f31904d5a6e480d4
katoragashua <katoragashua@outlook.com>
2025-07-21 23:35:39 +0100

    Web3app: Fixed app functionality

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import { useState } from "react";
 import abi from "./abi.json";
 import { ethers } from "ethers";
 
-const contractAddress = "0x9D1eb059977D71E1A21BdebD1F700d4A39744A70";
+const contractAddress = "0x59141CB9369bcf91068d0B4190Ae3DEa2725C803"; // Replace with your contract address
 
 function App() {
   const [text, setText] = useState("");
   const [error, setError] = useState("");
+  const [message, setMessage] = useState("");
 
   async function requestAccount() {
     try {
@@ -45,16 +46,45 @@ function App() {
     }
   };
 
+  const getMessage = async () => {
+    try {
+      if (window.ethereum) {
+        await requestAccount();
+        const provider = new ethers.BrowserProvider(window.ethereum);
+        const contract = new ethers.Contract(contractAddress, abi, provider);
+        const message = await contract.getMessage();
+        setMessage(message);
+        console.log("Current message:", message);
+        alert(`Current message: ${message}`);
+      } else {
+        console.error(
+          "MetaMask not found. Please install MetaMask to use this application."
+        );
+      }
+    } catch (error) {
+      console.error("Error getting message:", error);
+      alert(error.message || error);
+    }
+  };
+
   return (
     <div style={{ padding: "2rem" }}>
       <h1>Set Message on Smart Contract</h1>
-      <input
-        type="text"
-        placeholder="Set message"
-        value={text}
-        onChange={(e) => setText(e.target.value)}
-      />
-      <button onClick={handleSet}>Set Message</button>
+      <main className="main">
+        <div className="set-message">
+        <input
+          type="text"
+          placeholder="Set message"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        <button onClick={handleSet}>Set Message</button>
+      </div>
+      <div className="get-message">
+        <button onClick={getMessage}>Get Message</button>
+        <h2>{message}</h2>
+      </div>
+      </main>
     </div>
   );
 }
